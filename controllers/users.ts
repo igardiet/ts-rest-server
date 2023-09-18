8423b92ca127de1874effe5223393ce7e7a8320f
igardiet <igardiet@gmail.com>
2023-09-18 16:25:26 +0200

    app final commit

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,52 +1,106 @@
 import { Request, Response } from 'express';
+import User from '../models/user';
 
-export const getUsers = ( req: Request, res: Response ) =>
+export const getUsers = async ( req: Request, res: Response ) =>
 {
-    res.json(
-        {
-            msg: 'getUsers'
-        } )
+    const users = await User.findAll();
+    res.json( { users } )
 }
 
-export const getUser = ( req: Request, res: Response ) =>
+export const getUser = async ( req: Request, res: Response ) =>
 {
     const { id } = req.params;
+    const user = await User.findByPk( id );
 
-    res.json(
-        {
-            msg: 'getUser',
-            id
-        } )
+    if ( user )
+    {
+        res.json( { user } )
+    } else
+    {
+        res.status( 404 ).json(
+            {
+                msg: `There is no user with id nº ${ id }`
+            } )
+    }
 }
 
-export const postUser = ( req: Request, res: Response ) =>
+export const postUser = async ( req: Request, res: Response ) =>
 {
     const { body } = req;
-    res.json( {
-        msg: 'postUser',
-        body
-    } )
+
+    try
+    {
+        const emailExists = await User.findOne(
+            {
+                where: { email: body.email }
+            } )
+
+        if ( emailExists ) 
+        {
+            return res.status( 400 ).json(
+                {
+                    msg: `The email ${ body.email } is already registered`
+                } )
+        }
+
+        const user = await User.create( body );
+        res.json( { user } );
+
+    } catch ( error )
+    {
+        console.log( error );
+        res.status( 500 ).json( {
+            msg: 'Internal Server Error'
+        } )
+    }
 }
 
-export const putUser = ( req: Request, res: Response ) =>
+
+export const putUser = async ( req: Request, res: Response ) =>
 {
     const { id } = req.params;
     const { body } = req;
 
-    res.json(
+    try
+    {
+        const user = await User.findByPk( id );
+        if ( !user )
         {
-            msg: 'putUser',
-            body,
-            id
-        } )
+            return res.status( 404 ).json(
+                {
+                    msg: `There is no user with id nº ${ id }`
+                } );
+        }
+
+        await user.update( body );
+        res.json( user );
+
+    } catch ( error )
+    {
+        console.log( error );
+        res.status( 500 ).json(
+            {
+                msg: 'Internal Server Error'
+            }
+        )
+    }
 }
 
-export const deleteUser = ( req: Request, res: Response ) =>
+export const deleteUser = async ( req: Request, res: Response ) =>
 {
     const { id } = req.params;
+    const user = await User.findByPk( id );
 
-    res.json( {
-        msg: 'deleteUser',
-        id
-    } )
+    if ( !user )
+    {
+        return res.status( 404 ).json(
+            {
+                msg: `There is no user with id nº ${ id }`
+            } );
+    }
+
+    await user.update( { status: false } );
+
+
+    res.json( { user } )
 }
--- dist/controllers/users.js
+++ dist/controllers/users.js
@@ -1,44 +1,90 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUser = exports.putUser = exports.postUser = exports.getUser = exports.getUsers = void 0;
-const getUsers = (req, res) => {
-    res.json({
-        msg: 'getUsers'
-    });
-};
+const user_1 = __importDefault(require("../models/user"));
+const getUsers = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield user_1.default.findAll();
+    res.json({ users });
+});
 exports.getUsers = getUsers;
-const getUser = (req, res) => {
+const getUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    res.json({
-        msg: 'getUser',
-        id
-    });
-};
+    const user = yield user_1.default.findByPk(id);
+    if (user) {
+        res.json({ user });
+    }
+    else {
+        res.status(404).json({
+            msg: `There is no user with id nº ${id}`
+        });
+    }
+});
 exports.getUser = getUser;
-const postUser = (req, res) => {
+const postUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { body } = req;
-    res.json({
-        msg: 'postUser',
-        body
-    });
-};
+    try {
+        const emailExists = yield user_1.default.findOne({
+            where: { email: body.email }
+        });
+        if (emailExists) {
+            return res.status(400).json({
+                msg: `The email ${body.email} is already registered`
+            });
+        }
+        const user = yield user_1.default.create(body);
+        res.json({ user });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Internal Server Error'
+        });
+    }
+});
 exports.postUser = postUser;
-const putUser = (req, res) => {
+const putUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const { body } = req;
-    res.json({
-        msg: 'putUser',
-        body,
-        id
-    });
-};
+    try {
+        const user = yield user_1.default.findByPk(id);
+        if (!user) {
+            return res.status(404).json({
+                msg: `There is no user with id nº ${id}`
+            });
+        }
+        yield user.update(body);
+        res.json(user);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Internal Server Error'
+        });
+    }
+});
 exports.putUser = putUser;
-const deleteUser = (req, res) => {
+const deleteUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    res.json({
-        msg: 'deleteUser',
-        id
-    });
-};
+    const user = yield user_1.default.findByPk(id);
+    if (!user) {
+        return res.status(404).json({
+            msg: `There is no user with id nº ${id}`
+        });
+    }
+    yield user.update({ status: false });
+    res.json({ user });
+});
 exports.deleteUser = deleteUser;
 //# sourceMappingURL=users.js.map
